88660cd5b4eb6cdc40b70d031b000c8dd7aa5a78
Batuhan Aktaş <aktas.batu@hotmail.com>
2017-12-07 17:59:01 +0300

    Initial commit

---
+++ mediaplayer/Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace mediaplayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.wmp1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Btn_Ekle = new System.Windows.Forms.Button();
+            this.link = new System.Windows.Forms.Label();
+            this.adi = new System.Windows.Forms.Label();
+            this.Btn_gozat = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Btn_duzenle = new System.Windows.Forms.Button();
+            this.Btn_sil = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.gizlilabel1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wmp1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1007, 463);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            this.tabControl1.TabIndexChanged += new System.EventHandler(this.tabControl1_TabIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.wmp1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(999, 434);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Oynatıcı";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(749, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(247, 428);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // wmp1
+            // 
+            this.wmp1.Enabled = true;
+            this.wmp1.Location = new System.Drawing.Point(6, 3);
+            this.wmp1.MaximumSize = new System.Drawing.Size(737, 428);
+            this.wmp1.MinimumSize = new System.Drawing.Size(737, 428);
+            this.wmp1.Name = "wmp1";
+            this.wmp1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmp1.OcxState")));
+            this.wmp1.Size = new System.Drawing.Size(737, 428);
+            this.wmp1.TabIndex = 7;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.Btn_Ekle);
+            this.tabPage2.Controls.Add(this.link);
+            this.tabPage2.Controls.Add(this.adi);
+            this.tabPage2.Controls.Add(this.Btn_gozat);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(999, 434);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Medya Ekle";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Ekle
+            // 
+            this.Btn_Ekle.Location = new System.Drawing.Point(294, 225);
+            this.Btn_Ekle.Name = "Btn_Ekle";
+            this.Btn_Ekle.Size = new System.Drawing.Size(156, 61);
+            this.Btn_Ekle.TabIndex = 3;
+            this.Btn_Ekle.Text = "Ekle";
+            this.Btn_Ekle.UseVisualStyleBackColor = true;
+            this.Btn_Ekle.Click += new System.EventHandler(this.Btn_Ekle_Click);
+            // 
+            // link
+            // 
+            this.link.AutoSize = true;
+            this.link.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.link.Location = new System.Drawing.Point(84, 124);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(105, 24);
+            this.link.TabIndex = 2;
+            this.link.Text = "Dosya Yolu";
+            // 
+            // adi
+            // 
+            this.adi.AutoSize = true;
+            this.adi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.adi.Location = new System.Drawing.Point(84, 50);
+            this.adi.Name = "adi";
+            this.adi.Size = new System.Drawing.Size(95, 24);
+            this.adi.TabIndex = 1;
+            this.adi.Text = "Dosya Adı";
+            // 
+            // Btn_gozat
+            // 
+            this.Btn_gozat.Location = new System.Drawing.Point(88, 225);
+            this.Btn_gozat.Name = "Btn_gozat";
+            this.Btn_gozat.Size = new System.Drawing.Size(156, 61);
+            this.Btn_gozat.TabIndex = 0;
+            this.Btn_gozat.Text = "Gözat...";
+            this.Btn_gozat.UseVisualStyleBackColor = true;
+            this.Btn_gozat.Click += new System.EventHandler(this.Btn_gozat_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.Moccasin;
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.textBox2);
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.panel1);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.dataGridView1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(999, 434);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Listeyi Düzenle";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Dosya Yolu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dosya Adı";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 327);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(528, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 291);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Btn_duzenle);
+            this.panel1.Controls.Add(this.Btn_sil);
+            this.panel1.Location = new System.Drawing.Point(859, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(132, 237);
+            this.panel1.TabIndex = 2;
+            // 
+            // Btn_duzenle
+            // 
+            this.Btn_duzenle.Location = new System.Drawing.Point(14, 87);
+            this.Btn_duzenle.Name = "Btn_duzenle";
+            this.Btn_duzenle.Size = new System.Drawing.Size(105, 51);
+            this.Btn_duzenle.TabIndex = 1;
+            this.Btn_duzenle.Text = "Düzenle";
+            this.Btn_duzenle.UseVisualStyleBackColor = true;
+            this.Btn_duzenle.Click += new System.EventHandler(this.Btn_duzenle_Click);
+            // 
+            // Btn_sil
+            // 
+            this.Btn_sil.Location = new System.Drawing.Point(14, 17);
+            this.Btn_sil.Name = "Btn_sil";
+            this.Btn_sil.Size = new System.Drawing.Size(105, 51);
+            this.Btn_sil.TabIndex = 0;
+            this.Btn_sil.Text = "Şarkıyı Sil";
+            this.Btn_sil.UseVisualStyleBackColor = true;
+            this.Btn_sil.Click += new System.EventHandler(this.Btn_sil_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(700, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 54);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 12);
+            this.dataGridView1.MaximumSize = new System.Drawing.Size(850, 251);
+            this.dataGridView1.MinimumSize = new System.Drawing.Size(850, 251);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(850, 251);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tabPage4.Controls.Add(this.gizlilabel1);
+            this.tabPage4.Controls.Add(this.pictureBox1);
+            this.tabPage4.Controls.Add(this.label5);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(999, 434);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Hakkında";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(606, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(367, 258);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label5.Location = new System.Drawing.Point(50, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(501, 157);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Bu program Tuğçe Cambazoğlu tarafından tasarlanmıştır. İstenildiği gibi dağıtılab" +
+    "ilir.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // gizlilabel1
+            // 
+            this.gizlilabel1.AutoSize = true;
+            this.gizlilabel1.Location = new System.Drawing.Point(79, 242);
+            this.gizlilabel1.Name = "gizlilabel1";
+            this.gizlilabel1.Size = new System.Drawing.Size(46, 17);
+            this.gizlilabel1.TabIndex = 2;
+            this.gizlilabel1.Text = "label1";
+            this.gizlilabel1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 463);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1025, 510);
+            this.MinimumSize = new System.Drawing.Size(1025, 510);
+            this.Name = "Form1";
+            this.Text = "Tuğçe Media Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.wmp1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button Btn_gozat;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Btn_Ekle;
+        private System.Windows.Forms.Label link;
+        private System.Windows.Forms.Label adi;
+        private AxWMPLib.AxWindowsMediaPlayer wmp1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn_sil;
+        private System.Windows.Forms.Button Btn_duzenle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label gizlilabel1;
+    }
+}
+
---
+++ mediaplayer/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace mediaplayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string conString = "Server=.;Database=tugce_db;Integrated Security=True";     
+        //Bu veritabanına bağlanmak için gerekli olan bağlantı cümlemiz.Bu satıra dikkat edelim.
+        //Sql Servera bağlanırken kullandığımız bilgileri ve veritabanı ismini yazıyoruz.
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Form1 frm1 = new Form1();
+            listele();
+
+        }
+
+        private void listele()
+        {
+            SqlConnection baglanti = new SqlConnection(conString);
+            SqlCommand komut = new SqlCommand("SELECT * from tbl_sarkilar", baglanti);
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+                //bağlantı açıldı
+            }
+
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                listBox1.Items.Add(dr["sarkiAdi"]);
+                //şarkılar listeleniyor
+            }
+            baglanti.Close();
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+                //bağlantı açıldı
+            }
+            SqlCommand komut2 = new SqlCommand("SELECT * FROM tbl_sarkilar", baglanti);
+            SqlDataAdapter da = new SqlDataAdapter(komut2);
+            //SqlDataAdapter sınıfı verilerin databaseden aktarılması işlemini gerçekleştirir.
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            //Bir DataTable oluşturarak DataAdapter ile getirilen verileri tablo içerisine dolduruyoruz.
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection(conString);
+            string tiklanan = listBox1.SelectedItem.ToString();
+            //listboxtaki seçili elemanın linki değişkene kopyalandı.
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlCommand komut = new SqlCommand("SELECT link from tbl_sarkilar WHERE sarkiAdi='" + tiklanan + "'", baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                wmp1.URL = dr["link"].ToString();
+                //şarkı/video çalınıyor.
+            }
+            baglanti.Close();
+
+        }
+       
+  
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id= Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            SqlConnection baglanti = new SqlConnection(conString);
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlCommand komut = new SqlCommand("UPDATE tbl_sarkilar SET sarkiAdi=@sarkiAdi,link=@link where id=@id", baglanti);
+            //güncelleme komutu oluşturuldu
+            komut.Parameters.AddWithValue("@id", id);
+            komut.Parameters.AddWithValue("@sarkiAdi", textBox1.Text);
+            komut.Parameters.AddWithValue("@link", textBox2.Text);
+            //parametreler eklendi
+            komut.ExecuteNonQuery();
+            //komut çalıştırıldı
+            listele();
+            baglanti.Close();
+        }
+
+        private void Btn_sil_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection baglanti = new SqlConnection(conString);
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlCommand komut = new SqlCommand("DELETE FROM tbl_Sarkilar WHERE id=@id", baglanti);
+            //silme komutu oluşturuldu
+            komut.Parameters.AddWithValue("@id", dataGridView1.CurrentRow.Cells[0].Value);
+            //parametreler eklendi
+            komut.ExecuteNonQuery();
+            //komut çalıştırıldı
+            listele();
+        }
+
+
+        private void Btn_gozat_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog sarki = new OpenFileDialog();
+            //dosya seçim penceresi açıldı
+            sarki.Filter = "Genel medya dosyalar |*.mp3| |*.mp4| |*.wmv| |*.wma| |*.avi";
+            //dosya formatları
+            sarki.ShowDialog();
+            //medya seçildi
+            FileInfo dosyabilgisi = new FileInfo(sarki.FileName);
+            //özellikleri almak için nesne tanımlandı
+            adi.Text = dosyabilgisi.Name.ToString();
+            link.Text = dosyabilgisi.FullName.ToString();
+            //dosya özellikleri labellere alındı
+        }
+
+        private void Btn_Ekle_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection(conString);
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            //burada bağlantı açık mı kontrol etmeliyiz. Açık olan bağlantıyı açmaya çalışmamak için.
+            SqlCommand komut = new SqlCommand("INSERT into tbl_sarkilar(sarkiAdi, link) values(@sarki, @link)", baglanti);
+            //ekleme komutu gönderildi
+            komut.Parameters.AddWithValue("@sarki", adi.Text);
+            komut.Parameters.AddWithValue("@link", link.Text);
+            //parametreler eklendi
+            komut.ExecuteNonQuery();
+            //komut çalıştırıldı
+            listBox1.Items.Clear();
+            dataGridView1.Dispose();
+            listele();
+            baglanti.Close();
+            
+        }
+
+        private void Btn_duzenle_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell.ColumnIndex != 0)
+            {
+                textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                //Düzenlemek için tablodan verileri aldık.
+            }
+        }
+
+        private void tabControl1_TabIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            gizlilabel1.Text = "Tuğçe Cambazoğlu";
+            gizlilabel1.Visible = true;
+            //mouse resmin üstüne geldiğinde yazı görünür olacak.
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            gizlilabel1.Visible = false;
+            //mouse gittiğinde kaybolacak.
+        }
+    }
+}
